3d68a01303dcab5fbf71719d67268062e8070bcd
abdelazizmr <abdelazizmrabet.02@gmail.com>
2022-08-17 18:26:21 +0100

    modified style for small devices navbar

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ let menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click',()=>{
     document.querySelector('.mobile').classList.toggle('show')
 })
+
+window.addEventListener('scroll',()=>{
+    console.log('scrolling');
+    document.querySelector('.mobile').classList.remove('show')
+})
